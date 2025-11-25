ba74af10e9af96d632153f1d035d0ed7e44c42a0
Ssaif3 <samsaif933@gmail.com>
2025-11-25 15:13:25 +0530

    changwefbe

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://trading-omega-sepia.vercel.app",
     timeout: 5000,
     withCredentials: true,
     headers: {
