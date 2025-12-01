b53609f3cdd0ae6186e5d2d52cb2ad67c831d5aa
Ssaif3 <samsaif933@gmail.com>
2025-12-01 10:36:05 +0530

    hhbrgug

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://trading-omega-sepia.vercel.app/api",
+//   baseURL: "http://localhost:4000/api",
   timeout: 5000,
 //   withCredentials: false, // ❌ remove this, you don't use cookies
   headers: {
